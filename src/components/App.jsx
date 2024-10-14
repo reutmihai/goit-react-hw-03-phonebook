2746337aa56c537924a9cfdee45a0e947b9c0081
Reut Mihai <reutmihai@icloud.com>
2024-10-14 19:00:07 +0300

    transform App component in class

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,47 +1,29 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { nanoid } from 'nanoid';
 
-const App = () => {
-  const [state, setState] = useState({
+class App extends React.Component {
+  state = {
     contacts: [
-      {
-        id: nanoid(),
-        name: 'Reut Mihai',
-        number: '0753256355',
-      },
-      {
-        id: nanoid(),
-        name: 'Mironescu Andrei',
-        number: '0752526526',
-      },
-      {
-        id: nanoid(),
-        name: 'Cretan Cosmin',
-        number: '0746134620',
-      },
+      { id: nanoid(), name: 'Reut Mihai', number: '0753256355' },
+      { id: nanoid(), name: 'Mironescu Andrei', number: '0752526526' },
+      { id: nanoid(), name: 'Cretan Cosmin', number: '0746134620' },
     ],
     filter: '',
     name: '',
     number: '',
-  });
-
-  const handleChange = e => {
-    const { name, value } = e.target;
-    setState(prevState => ({
-      ...prevState,
-      [name]: value,
-    }));
   };
 
-  const handleSubmit = e => {
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = e => {
     e.preventDefault();
-    const newContact = {
-      id: nanoid(),
-      name: state.name,
-      number: state.number,
-    };
-    setState(prevState => ({
-      ...prevState,
+    const { name, number } = this.state;
+    const newContact = { id: nanoid(), name, number };
+
+    this.setState(prevState => ({
       contacts: [...prevState.contacts, newContact],
       name: '',
       number: '',
@@ -49,59 +31,67 @@ const App = () => {
     }));
   };
 
-  return (
-    <div>
-      <h1>Phonebook</h1>
-      <form onSubmit={handleSubmit}>
-        <span>Name</span>
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <form onSubmit={this.handleSubmit}>
+          <span>Name</span>
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            pattern="^[a-zA-Z]+(([' -][a-zA-Z ])?[a-zA-Z]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces."
+            required
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+          <span>Number</span>
+          <input
+            type="tel"
+            name="number"
+            placeholder="Number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +."
+            required
+            value={this.state.number}
+            onChange={this.handleChange}
+          />
+          <button type="submit">Add Contact</button>
+        </form>
+        <h2>Contacts</h2>
+        <ul>
+          {this.state.contacts.map(contact => (
+            <li key={contact.id}>
+              {contact.name} : {contact.number}
+            </li>
+          ))}
+        </ul>
+        <h3>Find contacts by name</h3>
         <input
           type="text"
-          name="name"
-          placeholder='Name'
-          pattern="^[a-zA-Z]+(([' -][a-zA-Z ])?[a-zA-Z]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-          value={state.name}
-          onChange={handleChange}
+          name="filter"
+          placeholder="Type a name"
+          value={this.state.filter}
+          onChange={this.handleChange}
         />
-        <span>Number</span>
-        <input
-          type="tel"
-          name="number"
-          placeholder='Number'
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-          value={state.number}
-          onChange={handleChange}
-        />
-        <button type="submit">Add Contact</button>
-      </form>
-      <h2>Contacts</h2>
-      <ul>
-        {state.contacts.map(contact => (
-          <li key={contact.id}>
-            {contact.name} : {contact.number}
-          </li>
-        ))}
-      </ul>
-      <h3>Find contacts by name</h3>
-      <input
-      type='text'
-      name="filter"
-      placeholder='Type a name'
-      value={state.filter}
-      onChange={handleChange}
-      />
-      <ul>
-        {state.contacts.filter(contact => contact.name.toLowerCase().includes(state.filter.toLowerCase())).map(contact => (
-          <li key={contact.id}>
-            {contact.name} : {contact.number}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+        <ul>
+          {this.state.contacts
+            .filter(contact =>
+              contact.name
+                .toLowerCase()
+                .includes(this.state.filter.toLowerCase())
+            )
+            .map(contact => (
+              <li key={contact.id}>
+                {contact.name} : {contact.number}
+              </li>
+            ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
